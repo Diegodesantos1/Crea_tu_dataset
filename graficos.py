0203d3538b8f00a96bea11f76c2b8c05b725cf8d
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-22 12:28:23 +0200

    añadidos los planetas

--- graficos.py
+++ graficos.py
@@ -4,12 +4,16 @@ class Graficos:
     def grafico():
         datos = pd.read_csv("Dataset.csv", sep =",")
         lista_gravedad = list(datos["gravity"])
-        gravedad_util = []
+        lista_nombres = list(datos["eName"])
+        gravedad_util, nombres_util = [], []
         print(lista_gravedad)
         for i in range (len(lista_gravedad)):
             if 8.0 < lista_gravedad[i] <10.5:
                 gravedad_util.append(lista_gravedad[i])
+                nombres_util.append(lista_nombres[i])
 
         print(gravedad_util)
+        print("son las gravedades correspondientes a los siguientes nombres:")
+        print(nombres_util)
 
 Graficos.grafico()
