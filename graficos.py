fb1fd6a840b4bedddbf046ab9e937a8ebda15f23
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-22 12:31:40 +0200

    añadido

--- graficos.py
+++ graficos.py
@@ -2,12 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 class Graficos:
     def grafico():
-        indices = []
         datos = pd.read_csv("Dataset.csv", sep =",")
         lista_gravedad = list(datos["gravity"])
-        gravedad_util = []
+        lista_nombres = list(datos["eName"])
+        gravedad_util, nombres_util = [], []
         print(lista_gravedad)
         for i in range (len(lista_gravedad)):
             if 8.0 < lista_gravedad[i] <10.5:
                 gravedad_util.append(lista_gravedad[i])
+                nombres_util.append(lista_nombres[i])
+
+        print(gravedad_util)
+        print("son las gravedades correspondientes a los siguientes nombres:")
+        print(nombres_util)
+
 Graficos.grafico()
