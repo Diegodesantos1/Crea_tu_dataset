e91d55bae05df21070e78763106ec43b89ba5ed4
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-22 13:02:11 +0200

    .

--- graficos.py
+++ graficos.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from pandas import DataFrame as df
 import matplotlib.pyplot as plt
+import numpy as np
 class Graficos:
     def grafico():
         datos = pd.read_csv("Dataset.csv", sep =",")
@@ -18,11 +20,13 @@ class Graficos:
         utiles_porcentaje = 0.01509433962
         no_utiles_porcentaje = 1 -utiles_porcentaje
         nombre = ["Gravedad válida", "Gravedad no válida"] ; parametros = [utiles_porcentaje, no_utiles_porcentaje] ; colores = ["#AAF683", "#EE6055"]
-        print (utiles_porcentaje, no_utiles_porcentaje)
         plt.subplot(1,2,1)
         plt.bar (nombres_util, gravedad_util, color = ["r", "g", "b", "y"])
         plt.subplot (1,2,2)
         plt.pie (parametros, labels=nombre, colors = colores)
         plt.show()
+        tierra = df.iloc[88]
+        print(tierra)
+        
 
 Graficos.grafico()
