e95ca66a7c489b4e2a61cdc515ae381e31ad096f
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-22 18:08:16 +0200

    cambios

--- graficos.py
+++ graficos.py
@@ -6,10 +6,8 @@ class Graficos:
     def grafico():
         datos = pd.read_csv("Dataset.csv", sep =",")
         datos_exo = pd.read_csv("PS_2022.04.22_04.00.41.csv", sep =",")
-        lista_gravedad = list(datos["gravity"])
-        lista_nombres = list(datos["eName"])
-        lista_gravedad_exo = list(datos_exo["st_logg"])
-        lista_nombres_exo = list(datos_exo["pl_name"])
+        lista_gravedad = list(datos["gravity"]) ; lista_nombres = list(datos["eName"])
+        lista_gravedad_exo = list(datos_exo["st_logg"]) ; lista_nombres_exo = list(datos_exo["pl_name"])
         gravedad_util, nombres_util = [], []
         print(lista_gravedad)
         print(lista_gravedad_exo)
@@ -37,9 +35,7 @@ class Graficos:
         venus = datos.iloc[261]
         saturno= datos.iloc[226]
         urano = datos.iloc[258]
-        print(f"\n Los datos de la tierra son {tierra}")
-        print(f"\n Los datos de venus son {venus}")
-        print(f"\n Los datos de saturno son {saturno}")
-        print(f"\n Los datos de urano son {urano}")
+        print(f"\n Los datos de la tierra son: \n {tierra}") ; print(f"\n Los datos de venus son: \n  {venus}")
+        print(f"\n Los datos de saturno son: \n {saturno}") ; print(f"\n Los datos de urano son: \n {urano}")
 
 Graficos.grafico()
