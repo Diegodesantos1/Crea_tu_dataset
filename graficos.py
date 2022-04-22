277b544d465d4b4c010d97d888f056d1574f9d36
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-22 12:48:51 +0200

    graficado

--- graficos.py
+++ graficos.py
@@ -15,5 +15,14 @@ class Graficos:
         print(gravedad_util)
         print("son las gravedades correspondientes a los siguientes nombres:")
         print(nombres_util)
+        utiles_porcentaje = 0.01509433962
+        no_utiles_porcentaje = 1 -utiles_porcentaje
+        nombre = ["Gravedad válida", "Gravedad no válida"] ; parametros = [utiles_porcentaje, no_utiles_porcentaje] ; colores = ["#AAF683", "#EE6055"]
+        print (utiles_porcentaje, no_utiles_porcentaje)
+        plt.subplot(1,2,1)
+        plt.bar (nombres_util, gravedad_util, color = ["r", "g", "b", "y"])
+        plt.subplot (1,2,2)
+        plt.pie (parametros, labels=nombre, colors = colores)
+        plt.show()
 
 Graficos.grafico()
